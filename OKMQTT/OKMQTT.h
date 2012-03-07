5601e78c4327af474e5ffb8f9b8d3a4692923da1
dpslwk <dps.lwk@gmail.com>
2012-03-07 07:48:03 +0000

    First OKMQTT example, completely untested

---
+++ OKMQTT/OKMQTT.h
@@ -0,0 +1,81 @@
+/****************************************************	
+ * sketch = OKMQTT
+ *
+ * Nottingham Hackspace
+ * CC-BY-SA
+ *
+ * Source = http://wiki.nottinghack.org.uk/wiki/...
+ * Target controller = Arduino 328 (Ciseco OpenKnotrol Gateway)
+ * Clock speed = 16 MHz
+ * Development platform = Arduino IDE 1.0
+ * C compiler = WinAVR from Arduino IDE 1.0
+ * 
+ * 
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * Simple LLAP<->MQTT bridge example for the Ciseco Open
+ *
+ * 
+ ****************************************************/
+
+/*
+ These are the global config parameters
+ Including Pin Outs, IP's, TimeOut's etc
+ 
+ */
+
+#ifndef  OKMQTT_h
+#define  OKMQTT_h
+
+/**************************************************** 
+ * genral defines
+ * use for staic config bits
+ ****************************************************/
+// baud rate for the XRF
+#define XRF_BAUD 9600
+
+//LLAP messgaes are 12 char's long + NULL terminator
+#define LLAP_BUFFER_LENGHT
+
+
+/**************************************************** 
+ * Ethernet settings
+ * Update these with values suitable for your network.
+ *
+ ****************************************************/
+
+byte mac[] = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xAF }; // Grab the mac from your wiznet chip
+
+//uncomment and fill in for static ip
+//IPAddress ip(0,0,0,0);
+
+
+/**************************************************** 
+ * MQTT settings
+ *
+ ****************************************************/
+
+// MQTT server 
+IPAddress mqttIp(10,0,0,2);
+#define MQTT_PORT 1883
+
+// ClientId for connecting to MQTT
+#define CLIENT_ID "OpenKnotrol"
+
+// Subscribe to topics
+#define S_RX		"ok/tx/#"
+#define S_RX_MASK   "ok/tx/"
+
+// Publish Topics
+#define P_TX		"ok/rx"
+
+// Status Topic, use to say we are alive or DEAD (will)
+#define S_STATUS "ok/status"
+#define P_STATUS "ok/status"
+#define STATUS_STRING "STATUS"
+#define RUNNING "Running: OKMQTT"
+#define RESTART "Restart: OKMQTT"
+
+#endif
---
+++ OKMQTT/OKMQTT.ino
@@ -0,0 +1,201 @@
+/****************************************************	
+ * sketch = OKMQTT
+ *
+ * Nottingham Hackspace
+ * CC-BY-SA
+ *
+ * Source = http://wiki.nottinghack.org.uk/wiki/...
+ * Target controller = Arduino 328 (Ciseco OpenKontrol Gateway)
+ * Clock speed = 16 MHz
+ * Development platform = Arduino IDE 1.0
+ * C compiler = WinAVR from Arduino IDE 1.0
+ * 
+ * 
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * Simple LLAP<->MQTT bridge example for the Ciseco OpenKontrol Gateway
+ *
+ * 
+ ****************************************************/
+
+/*  
+ History
+    000 - Started 07/03/2012
+ 	001 - Initial release
+			frist pass at a basic bridge
+            assumes XRF is on hardware UART and using wiznet ethernet
+            LLAP message have a max length of 12 char's
+            need to publish each LLAP individually via MQTT
+ 
+ Known issues:
+
+ 
+ Future changes:
+
+ 
+ ToDo:
+
+ 
+ Authors:
+ 'RepRap' Matt      dps.lwk at gmail.com
+
+ */
+
+#define VERSION_NUM 001
+#define VERSION_STRING "OKMQTT ver: 001"
+
+// Uncomment for debug prints
+#define DEBUG_PRINT
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+#include "OKMQTT.h"
+
+/**************************************************** 
+ * global vars
+ * 
+ ****************************************************/
+PubSubClient client(mqttIp, MQTT_PORT, callbackMQTT);
+char LLAPmsg[LLAP_BUFFER_LENGHT];
+
+/**************************************************** 
+ * pushXRF 
+ * Read incoming LLAP from MQTT and push to XRF 
+ ****************************************************/
+void pushXRF(char* topic, byte* payload, int length) {
+    // basic implementation
+    
+    // clear the buffer
+    memset(LLAPmsg, 0, LLAP_BUFFER_LENGHT);
+    // little memory overflow protection
+    if ((length) > LLAP_BUFFER_LENGHT-1) {
+        length = LLAP_BUFFER_LENGHT-1;
+    }
+    memcpy(LLAPmsg, payload, length);
+    
+    Serial.print(LLAPmsg);
+} 
+
+/**************************************************** 
+ * statusUpdate
+ * given MQTT request for STATUS respond as needed
+ *
+ ****************************************************/
+void statusUpdate(byte* payload, int length) {
+    // check for Status request
+    if (strncmp(STATUS_STRING, (char*)payload, strlen(STATUS_STRING)) == 0) {
+#ifdef DEBUG_PRINT
+        Serial.println("Status Request");
+#endif
+        client.publish(P_STATUS, RUNNING);
+    }
+}
+
+/**************************************************** 
+ * callbackMQTT
+ * called when we get a new MQTT
+ * work out which topic was published to and handel as needed
+ ****************************************************/
+void callbackMQTT(char* topic, byte* payload, int length) {
+    if (strncmp(S_RX_MASK, topic, strlen(S_RX_MASK)) == 0) {
+        pushXRF(topic, payload, length);
+    } else  if (!strcmp(S_STATUS, topic)) {
+        statusUpdate(payload, length);
+    }
+}
+
+/**************************************************** 
+ * pollXRF 
+ * Read incoming LLAP from XRF and push to MQTT 
+ ****************************************************/
+void pollXRF() {
+    if (Serial.available() >= 12){
+        if (Serial.read() == 'a') {
+            //clear the buffer
+            memset(LLAPmsg, 0, LLAP_BUFFER_LENGHT);
+            int pos = 0;
+            LLAPmsg[pos++] = 'a';
+            while (pos < 12) {
+                LLAPmsg[pos++] = Serial.read();
+            }
+            client.publish(P_TX, LLAPmsg);
+        }
+    }
+} 
+
+/**************************************************** 
+ * check we are still connected to MQTT
+ * reconnect if needed
+ *  
+ ****************************************************/
+void checkMQTT()
+{
+  	if(!client.connected()){
+		if (client.connect(CLIENT_ID)) {
+			client.publish(P_STATUS, RESTART);
+			client.subscribe(S_RX);
+			client.subscribe(S_STATUS);
+#ifdef DEBUG_PRINT
+			Serial.println("MQTT Reconnect");
+#endif
+		}
+	}
+} 
+
+
+void setup() {
+    // Start Serial
+	Serial.begin(XRF_BAUD);
+#ifdef DEBUG_PRINT
+	Serial.println(VERSION_STRING);
+#endif
+    
+    // Start ethernet on the OpenKontrol Gateway
+    // handels static ip or DHCP automatically
+    if(ip){
+        Ethernet.begin(mac, ip);
+    } else {    
+        if (Ethernet.begin(mac) == 0) {
+#ifdef DEBUG_PRINT
+            Serial.println("Failed to configure Ethernet using DHCP");
+#endif
+            // no point in carrying on, so do nothing forevermore:
+            for(;;)
+                ;
+        }
+    }
+    
+    // Setup Pins
+    
+    // Set default output's
+    
+    // Start MQTT and say we are alive
+    checkMQTT();
+    
+    // let everything else settle
+    delay(100);
+}
+
+void loop() {
+    // poll XRF for incoming LLAP
+	pollXRF();
+	
+	// Poll MQTT
+	// should cause callback if theres a new message
+	client.loop();
+
+	// are we still connected to MQTT
+	checkMQTT();
+
+}
+
+
+
+
+
+
+
+
